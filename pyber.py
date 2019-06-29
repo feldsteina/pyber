e3a08a96ce7e5592bd6fd6b31b36b8f70f93c922
Adam F <46360510+feldsteina@users.noreply.github.com>
2019-06-29 17:58:38 -0500

    major cleanup, corrected input data, final commit

--- pyber.py
+++ pyber.py
@@ -18,139 +18,96 @@ city_df = pd.read_csv(city_file)
 ride_df = pd.read_csv(ride_file)
 
 # Combine the data into a single dataset
-pyber_df = pd.merge(city_df, ride_df, on="city", how="inner")
-
-# Change city type to number, add city type name column
-pyber_df["city_type_name"] = pyber_df["type"]
-
-city_type_names = []
-
-for items in pyber_df.city_type_name.unique():
-    city_type_names.append(items)
-    print(items)
-
-city_type_cnt = pyber_df["city_type_name"].nunique()
-
-value_list = []
-
-for i in range(city_type_cnt):
-    value_list.append(i)
-    print(i)
-
-pyber_df["type"].replace(to_replace=city_type_names,
-                         inplace=True,
-                         value=value_list)
+pyber_df = pd.merge(city_df, ride_df, on="city", how="right")
 
 # Display the data table for preview
 pyber_df.head()
 
 # %%
-averages_df = pyber_df.groupby(["city"], as_index=False).mean()
+urban_cities = pyber_df[pyber_df.type == "Urban"]
+urban_ride_count = urban_cities.groupby(["city"]).count()["ride_id"]
+urban_driver_count = urban_cities.groupby(["city"]).mean()["driver_count"]
+urban_fare = urban_cities.groupby(["city"]).mean()["fare"]
 
-averages_df["ride_count"] = pyber_df.groupby(["city"])\
-    .count()["ride_id"].tolist()
+suburban_cities = pyber_df[pyber_df.type == "Suburban"]
+suburban_ride_count = suburban_cities.groupby(["city"]).count()["ride_id"]
+suburban_driver_count = suburban_cities.groupby(["city"]).mean()[
+    "driver_count"]
+suburban_fare = suburban_cities.groupby(["city"]).mean()["fare"]
 
-averages_df["driver_count"] = pyber_df.groupby(["city"])\
-    .median()["driver_count"].tolist()
-
-averages_df["city_type"] = pyber_df.groupby(["city"])\
-    .first()["type"].tolist()
-
-averages_df["city_type_name"] = pyber_df.groupby(["city"])\
-    .first()["city_type_name"].tolist()
-
-averages_df.drop(labels=["ride_id"], axis=1, inplace=True)
-
-averages_df["fare"] = averages_df["fare"].round(2)
-
-averages_df.head()
-
+rural_cities = pyber_df[pyber_df.type == "Rural"]
+rural_ride_count = rural_cities.groupby(["city"]).count()["ride_id"]
+rural_driver_count = rural_cities.groupby(["city"]).mean()["driver_count"]
+rural_fare = rural_cities.groupby(["city"]).mean()["fare"]
 
 # %% [markdown]
 # ## Bubble Plot of Ride Sharing Data
-
-# %%
-# Build a colormap
-
-cmap = ["gold", "lightblue", "coral"]
-
-cmapList = [cmap[i] for i in range(city_type_cnt)]
-
-cmap = colors.LinearSegmentedColormap.from_list(
-    "customCmap", cmapList, city_type_cnt)
-
-cmapList = [cmap(i) for i in range(city_type_cnt)]
-
-patch_data = []
-
-for i in range(city_type_cnt):
-    for items in city_type_names:
-        patch_data.append(
-            mpatches.Patch(
-                color=cmapList[i], label=city_type_names[i]
-            )
-        )
-        break
-
-# %%
-# Obtain the x and y coordinates for each of the three city types
-x = averages_df["driver_count"]
-y = averages_df["fare"]
-s = [n**2 for n in averages_df["ride_count"]]
-city_pop = averages_df["city_type"]
-
 # Build the scatter plots for each city types
-plt.scatter(x, y, s=s, c=city_pop, alpha=0.3, cmap=cmap)
+plt.scatter(urban_ride_count,
+            urban_fare,
+            s=urban_driver_count**2, c="lightcoral",
+            edgecolor="black", linewidths=1, marker="o",
+            alpha=0.4, label="Urban")
+
+plt.scatter(suburban_ride_count,
+            suburban_fare,
+            s=suburban_driver_count**2, c="lightskyblue",
+            edgecolor="black", linewidths=1, marker="o",
+            alpha=0.4, label="Suburban")
+
+plt.scatter(rural_ride_count,
+            rural_fare,
+            s=rural_driver_count**2, c="gold",
+            edgecolor="black", linewidths=1, marker="o",
+            alpha=0.4, label="Rural")
 
 # Incorporate the other graph properties
+plt.xlabel("Number of Rides")
+plt.ylabel("Average Fare ($)")
+plt.title("Average Fare vs Number of Rides per City (2016)")
+plt.grid(True)
 
 # Create a legend
+lgnd = plt.legend(fontsize="small", mode="Expanded",
+                  numpoints=1, scatterpoints=1,
+                  loc="best", title="City Types",
+                  labelspacing=0.5)
+lgnd.legendHandles[0]._sizes = [30]
+lgnd.legendHandles[1]._sizes = [30]
+lgnd.legendHandles[2]._sizes = [30]
 
 # Incorporate a text label regarding circle size
-# plt.labels("Bigger circle = more fares")
-plt.xlabel("Number of drivers per city")
-plt.ylabel("Average fare per city")
-plt.title("Average fare vs number of drivers per city")
-plt.text(20, 41, "Bigger circle = more fares")
-plt.legend(handles=patch_data)
+plt.text(42, 40, "Bigger circle = more rides")
 
 # Save Figure
-plt.savefig("bubble.png")
+plt.savefig("analysis/bubble.png")
 plt.show()
+
+# %%
+# set up variables for next section
+colors = ["gold", "lightskyblue", "lightcoral"]
+labels = ["Rural", "Suburban", "Urban"]
 
 # %% [markdown]
 # ## Total Fares by City Type
 
-
 # %%
 # Calculate Type Percents
-city_fare_pct = []
-
-for i in range(city_type_cnt):
-    city_fare_pct.append(
-        int(
-            round(
-                pyber_df.loc[
-                    pyber_df.type == i, ["fare"]
-                ].sum() /
-                pyber_df.fare.sum(), 2
-            ) * 100
-        )
-    )
-
-for items in city_fare_pct:
-    print(items)
+type_pct = 100 * \
+    pyber_df.groupby(["type"]).sum()["fare"] / pyber_df["fare"].sum()
 
 # %%
 # Build Pie Chart
-x = city_fare_pct
-
-plt.pie(x, labels=city_type_names, colors=cmapList, autopct="%.2f")
-
-plt.xlabel("% Fares by City Type")
-plt.legend()
-
-plt.savefig("fares_pie.png")
+plt.pie(type_pct,
+        labels=labels,
+        colors=colors,
+        explode=[0.0, 0.0, 0.1],
+        autopct="%1.2f%%",
+        shadow=True,
+        startangle=-43
+        )
+plt.title("% of Total Fares by City Type")
+plt.savefig("analysis/fares_pie.png")
 plt.show()
 
 # %% [markdown]
@@ -158,68 +115,49 @@ plt.show()
 
 # %%
 # Calculate Ride Percents
-city_total_rides_pct = []
-
-for i in range(city_type_cnt):
-    city_total_rides_pct.append(
-        int(
-            round(
-                averages_df.loc[
-                    averages_df.city_type == i, ["ride_count"]
-                ].sum() / averages_df["ride_count"].sum(), 2
-            ) * 100
-        )
-    )
-
-for items in city_total_rides_pct:
-    print(items)
+type_pct = 100 * \
+    pyber_df.groupby(["type"]).count()["ride_id"] / pyber_df["ride_id"].count()
 
 # %%
 # Build Pie Chart
-x = city_total_rides_pct
-
-plt.pie(x, labels=city_type_names, colors=cmapList, autopct="%.2f")
-plt.xlabel("% Rides by City Type")
-
-plt.legend()
-
-plt.savefig("rides_pie.png")
+plt.pie(type_pct,
+        labels=labels,
+        colors=colors,
+        explode=[0.0, 0.0, 0.1],
+        autopct="%1.2f%%",
+        shadow=True,
+        startangle=-23
+        )
+plt.title("% of Total Rides by City Type")
+plt.savefig("analysis/rides_pie.png")
 plt.show()
 
 # %% [markdown]
-# ## Total Drivers by City Type
+# ## Total Rides by City Type
 
 # %%
 # Calculate Driver Percents
-city_driver_pct = []
 
-for i in range(city_type_cnt):
-    city_driver_pct.append(
-        int(
-            round(
-                averages_df.loc[
-                    averages_df["city_type"] == i, ["driver_count"]
-                ].sum() / averages_df["driver_count"].sum(), 2
-            ) * 100
-        )
-    )
-
-for items in city_driver_pct:
-    print(items)
+type_pct = 100 * \
+    pyber_df.groupby(["type"]).sum()["driver_count"] / \
+    pyber_df["driver_count"].sum()
 
 # %%
 # Build Pie Charts
-x = city_driver_pct
-
-plt.pie(x, labels=city_type_names, colors=cmapList, autopct="%.2f")
-plt.xlabel("% Drivers by City Type")
-plt.legend()
-
-plt.savefig("drivers_pie.png")
+plt.pie(type_pct,
+        labels=labels,
+        colors=colors,
+        explode=[0.0, 0.0, 0.1],
+        autopct="%1.2f%%",
+        shadow=True,
+        startangle=43
+        )
+plt.title("% of Total Drivers by City Type")
+plt.savefig("analysis/drivers_pie.png")
 plt.show()
 
 # %% [markdown]
 # ## Observations
 # 1. Most of the drivers, rides, and fares are in Urban locations
-# 2. Urban areas have more drivers than they have rides
-# 3. Suburban areas have more rides than they have drivers
+# 2. Urban areas have a lower ratio of rides to drivers
+# 3. Rural areas make up the lowest market share, and have almost no drivers
